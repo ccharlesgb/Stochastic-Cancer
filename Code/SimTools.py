8af6f4ad967fb2a5357903109b890d5c53283276
Connor Charles <ccharles.gb@gmail.com>
2014-10-06 17:06:09 +0100

    Added simulation callback to gillespie
    You can now modify parameters as a function of time. (See gillespie.py
    modifyU1(sim) as an example usage

--- Code/SimTools.py
+++ Code/SimTools.py
@@ -6,9 +6,12 @@ random.seed()
 #Class for simulating cancer dynamics with the Gillespie algorithm
 class Gillespie:
     def __init__(self, numCells):
+        #Sim time parameters
         self.curTime = 0.0
         self.timeLimit = 100.0
         self.simSteps = 0
+        
+        self.populationHistory = 0        
         
         #The fitness
         self.r0 = 1.0
@@ -26,22 +29,19 @@ class Gillespie:
         self.n1 = 0
         self.n2 = 0
 
-        self.cells = [] #Is this actually needed?
-        for i in range(0, self.N):
-            self.cells.insert(i,0)
-
         self.lambd = self.GetLambda()
+        
+        self.preSim = 0
 
+    #Reset parameters to default values
     def ResetSim(self):
         self.curTime = 0.0
         self.simSteps = 0
         self.n0 = self.N
         self.n1 = 0
         self.n2 = 0
-        self.cells = []
-        for i in range(0, self.N):
-            self.cells.insert(i,0)
-            
+    
+    #Helper function to renormalize fitness of current cell population
     def AvgFitness(self):
         return (self.r0*self.n0 + self.r1*self.n1 + self.r2 * self.n2)
         
@@ -80,7 +80,8 @@ class Gillespie:
         return 1.0/self.GetLambda() * math.log(1.0/random.random())
         #return random.expovariate(self.GetLambda())
         
-    #Chose and execute which event to carry out. Updates cell array and population counts
+    #Chose and execute which event to carry out. Updates population counts
+    #Uses weighted random number between 0 and 1
     def ChooseEvent(self):
         rand = random.random()
         lam = self.GetLambda()
@@ -88,8 +89,6 @@ class Gillespie:
         threshold = self.GetT10()/lam
         #print(threshold)
         if (rand < threshold):
-            index = self.GetFirstIndexOf(1)
-            self.cells[index] = 0
             self.n0 += 1
             self.n1 -= 1
             return
@@ -97,8 +96,6 @@ class Gillespie:
         threshold += self.GetT20()/lam
         #print(threshold)
         if (rand < threshold):
-            index = self.GetFirstIndexOf(2)
-            self.cells[index] = 0
             self.n0 += 1
             self.n2 -= 1
             return
@@ -106,8 +103,6 @@ class Gillespie:
         threshold += self.GetT01()/lam
         #print(threshold)
         if (rand < threshold): 
-            index = self.GetFirstIndexOf(0)
-            self.cells[index] = 1
             self.n1 += 1
             self.n0 -= 1
             return
@@ -115,8 +110,6 @@ class Gillespie:
         threshold += self.GetT21()/lam
         #print(threshold)
         if (rand < threshold):
-            index = self.GetFirstIndexOf(2)
-            self.cells[index] = 1
             self.n1 += 1
             self.n2 -= 1
             return
@@ -124,8 +117,6 @@ class Gillespie:
         threshold += self.GetT02()/lam
         #print(threshold)
         if (rand < threshold): 
-            index = self.GetFirstIndexOf(0)
-            self.cells[index] = 2
             self.n2 += 1
             self.n0 -= 1
             return
@@ -133,28 +124,25 @@ class Gillespie:
         threshold += self.GetT12()/lam
         #print(threshold)
         if (rand < threshold): 
-            index = self.GetFirstIndexOf(1)
-            self.cells[index] = 2
             self.n2 += 1
             self.n1 -= 1
             return
-
-    #Helper function finds the first instance of a type of cell in the cell array
-    def GetFirstIndexOf(self, celltype):
-        for i in range(0, self.N):
-            if self.cells[i] == celltype:
-                return i
-
+    
+    #Returns true if we are at the absorbing state of n2 == N
     def Fixated(self):
         return self.n2 >= self.N
-    
+        
+    #Simulate loop
+    #Uses standard gillespie algorithm and chooses event until fixated or out of time
     def Simulate(self):
         self.ResetSim()
         while self.curTime < self.timeLimit:
-            timestep = self.GetTimeStep()
-            self.ChooseEvent()
-            if self.Fixated():
+            if self.preSim:
+                self.preSim(self)
+            timestep = self.GetTimeStep() #How much time until the next event?
+            self.ChooseEvent() #Chose what kind of event and update cell counts
+            if self.Fixated(): #Are we at absorbing state? If so quit
                 return
-            self.curTime += timestep
-            self.simSteps+= 1
+            self.curTime += timestep #Increment time
+            self.simSteps+= 1 #Increase event count
 #END CLASS DEFINTION
--- Code/gillespie.py
+++ Code/gillespie.py
@@ -2,13 +2,26 @@ import matplotlib.pyplot as plt
 import SimTools
 import time
 
+#Initialize the Gillespie simulator with 10 cells
 mySim = SimTools.Gillespie(10)
 mySim.timeLimit = 100
 mySim.u2 = 0.01
 dataPointCount = 25
 
-simsPerDataPoint = 1000
+#Pre Simulate callback (called every frame before a timestep)
+def modifyU1(sim):
+    if sim.curTime < 10.0:
+        sim.u1 = 0.2
+    else:
+        sim.u1 = 0.1
 
+mySim.preSim = modifyU1
+
+#Sweep the parameter r1 from 0.2 to 3.0 and run many simulations per data point
+#Gets an idea on how likely cancer fixation is to occur for this parameter
+simsPerDataPoint = 500
+
+#Initialize the array with default values
 dataPointsX = []
 dataPointsY = []
 for i in range(0, dataPointCount):
@@ -16,24 +29,29 @@ for i in range(0, dataPointCount):
     dataPointsY.insert(i,0.0)
 
 for curPoint in range(0, dataPointCount):
+    startTime = time.clock() #Algorithm benchmarking
+    
     fixationCounts = 0
-    startTime = time.clock()
     mySim.r1 = 0.2 + (3.0-0.2) * float(curPoint)/float(dataPointCount) #sweep type-1 fitness from 0.2 to 3.0
+    
     print("Current Data Point = {0}/{1} ({2}%)".format(curPoint + 1, dataPointCount, 100.0 * float(curPoint+1.0)/dataPointCount))
+    #Perform many simulations to get an accurate probability of the fixation probability
     for sim in range(0, simsPerDataPoint):
         mySim.Simulate()
             
-        if mySim.n2 == mySim.N:
+        if mySim.n2 == mySim.N: #The simulation ended with fixation
             fixationCounts += 1
+    #Once the loop is done get the fraction of fixations for this r1
     dataPointsX[curPoint] = mySim.r1
     dataPointsY[curPoint] = float(fixationCounts) / float(simsPerDataPoint)
 
     print("Complete (Took {:.{s}f} seconds)".format(time.clock() - startTime, s=2))
 
+#Dump data to console
 for i in range(0, dataPointCount):
     print("r1: {0} Fixation: {1}".format(dataPointsX[i],dataPointsY[i]))
 
-
+#Create graph of data
 plt.plot(dataPointsX, dataPointsY)
 plt.xlabel("Type 1 Fitness r1")
 plt.ylabel("Type 2 Fixation %")
@@ -44,6 +62,7 @@ filename = "sim_N={0}_r0={1}_r1={2}_r2={3}_u1={4}_u2={5}_SPDP={6}".format(mySim.
 
 f = open(filename, 'w')
 
+#Dump data to file
 for i in range(0, dataPointCount):
     f.write(str(dataPointsX[i]) + "    " + str(dataPointsY[i]) + "\n")
 
