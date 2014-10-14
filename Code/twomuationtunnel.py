307e12c28b1a1bf7d63a59f07215f4fbbd72c368
jnyadams <adams.jonny@gmail.com>
2014-10-14 12:40:18 +0100

    heano model now using vi function

--- Code/twomuationtunnel.py
+++ Code/twomuationtunnel.py
@@ -9,6 +9,7 @@ import scipy.integrate as integrate
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+import Vi
 
 datapointcount=100
 N=100
@@ -17,40 +18,14 @@ r1=1.1
 r2=1.0
 u1=0.1
 u2=0.01
-V1=0.7
 curpoint=100
 
 rho1=(1.0-(r0*(1.0-u1)/(r1+r0*u1)))/(1.0-math.pow((r0*(1.0-u1)/(r1+r0*u1)) , N))
 rho3=(1.0-(r0*(1.0-u1)/(r2+r1*u2)))/(1.0-math.pow((r0*(1.0-u1)/(r2+r1*u2)) , N))
 
-V = []
-V_lin = []
-
-for i in range(0,N):
-    V.append(1.0 - float(i+1) / N)
-    V_lin.append(V[i])
 
 
-MAX_ITER = 1000
-MIN_CHANGE = 1e-6
-
-V1_old = V[1]
-
-for it in range(0, MAX_ITER):
-    for i in range(1, N-1):
-        nxt = V[i+1]
-        prv = V[i-1]
-        P_i = float(i*(N-i)) / (i*r1*(1.0-u2) + r0*(N-i))
-        new = (r1*(1-u2) * nxt + r0 * prv)
-        new = new / ((r1*u2*rho3)/P_i + r1*(1-u2) + r0)
-        V[i] = new
-    
-    if abs(V[1] - V1_old) < MIN_CHANGE:
-        print("Converged in {0} steps.".format(i))
-        break
-    V1_old = V[1]
-
-V1=V[1]
+V1=Vi.GetV_i(1, r0, r1, r2, u1, u2, N)
 
 a=N*u1*rho1
 b=N*u1*(1.0-V1-rho1)
