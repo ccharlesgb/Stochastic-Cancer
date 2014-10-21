e43939a5fbd3ca46f10e13099c44ca5625a01188
Connor Charles <ccharles.gb@gmail.com>
2014-10-21 14:48:11 +0100

    qk doesnt work

--- Code/qk.py
+++ Code/qk.py
@@ -4,31 +4,33 @@ import math
 
 def GetLambdaK(k, N, r1, r2, u2):
     res = (N - k) * ((N-k)*r1*u2 + r2*k) / ((N-k)*r1 + r2*k)
-    
     return res
 
 
 def GetMuK(k, N, r1, r2, u2):
     res = k * ((N-k)*r1*(1-u2)) / ((N-k)*r1 + r2*k)
-    
     return res
 
 
-def GetQk(t, r0, r1, r2, u1, u2, N):
+def GetQ0(t, r0, r1, r2, u1, u2, N):
+    return GetQk(0, t, r0, r1, r2, u1, u2, N)
+
+def GetQk(k_return, t, r0, r1, r2, u1, u2, N):
     q = []
+    q_dots = []
     q_lin = []
     
     for i in range(0,N+1):
-        q.append((float(i) / N))
+        q.append(0.0)
+        q_dots.append(0.0)
         q_lin.append(q[i])
     
-    
+    q[N] = 1.0
     delta_t = 0.1
     MAX_ITER = int(t / delta_t)
-    MIN_CHANGE = 1e-5
-    
+
     curt = 0.0
-    for it in range(0, MAX_ITER):
+    for it in range(0, MAX_ITER + 1):
         for k in range(0, N):
             nextq = q[k + 1]
             if k > 0:
@@ -42,32 +44,50 @@ def GetQk(t, r0, r1, r2, u1, u2, N):
             else:
                 curmu = 0.0
             
-            qkdot = curlambda * nextq - (curlambda + curmu)*curq + curmu * prevq
-            newQ = q[k] + qkdot * delta_t
-            q[k] = newQ
+            q_dots[k] = curlambda * nextq - (curlambda + curmu)*curq + curmu * prevq
         
-        curt = it * delta_t
+        if (it == MAX_ITER):
+            delta_t = t - MAX_ITER * delta_t
+            #print("Final Step: {0}".format(delta_t))
+        curt = curt + delta_t
+        for k in range(0,N):
+            newQ = q[k] + q_dots[k] * delta_t
+            q[k] = newQ
     
-    return q[0]
-           
-datapointcount = 25           
+   # print("Final T {0}   {1}".format(curt, t))
+    return q[k_return]
+         
+datapointcount = 100           
            
 dataPointsq=[]
 dataPointst=[]
 dataPointsG = []
 
+dataPointsk = []
+dataPointsQk = []
+
 t = 100.0
 r0 = 1.0
-r1 = 1.01
+r1 = 0.5
 r2 = 1.0
 u1 = 0.1
 u2 = 0.1
 
 N = 10
 
+
+for k in range(0, N + 1):
+    dataPointsk.append(k)
+    dataPointsQk.append(GetQk(k, t, r0, r1, r2, u1, u2, N))
+    
+plt.figure()
+plt.plot(dataPointsk, dataPointsQk)
+
+plt.figure()
+
 for i in range(0, datapointcount):
     t = float(i) / (datapointcount - 1) * 100.0
-    q = GetQk(t, r0, r1, r2 ,u1 ,u2, N)
+    q = GetQk(0, t, r0, r1, r2 ,u1 ,u2, N)
     dataPointsq.append(q)
     dataPointst.append(t)
     
@@ -75,6 +95,7 @@ for i in range(0, datapointcount):
     curG = 1.0 - math.exp(-c*t)
     dataPointsG.append(curG)
     
-plt.plot(dataPointst, dataPointsq)
-plt.plot(dataPointst, dataPointsG)
+plt.plot(dataPointst, dataPointsq, label = "qk")
+plt.plot(dataPointst, dataPointsG, label = "G(t)", ls = '--')
+plt.legend()
 plt.show()
--- Code/twomuationtunnel.py
+++ Code/twomuationtunnel.py
@@ -10,15 +10,18 @@ import math
 import matplotlib.pyplot as plt
 #import numpy as np
 import Vi
+import qk
 
-datapointcount=25
+datapointcount=15
 N=10
 r0=1.0
-r1=0.0
+r1=0.3
 r2=1.0
 u1=0.1
-u2=0.01
+u2=0.1
 curpoint=100
+
+t = 100.0
 
 rho1 = 0.0
 rho3 = 0.0
@@ -26,6 +29,31 @@ V1 = 0.0
 a = 0.0
 b = 0.0
 c = 0.0
+
+q0_points = 100
+q0_vals = []
+
+def CacheQ0():
+    global q0_vals
+    q0_vals = []
+    for i in range(0, q0_points):
+        curT = float(i)/(q0_points-1) * t
+        q0_vals.append(qk.GetQ0(curT, r0, r1, r2, u1, u2, N))
+        
+def FetchQ0(time):
+    index = (time / t) * (q0_points -1)
+    #print("INDEX {0}".format(index))
+    
+    left_dex = int(math.floor(index))
+    right_dex = int(math.ceil(index))
+    left_val = q0_vals[left_dex]
+    right_val = q0_vals[right_dex]
+    
+    frac = index - left_dex
+    
+    val = left_val + (right_val - left_val) * frac
+    
+    return val
 
 def CalculateValues():
     global rho1
@@ -40,17 +68,18 @@ def CalculateValues():
 
     rho3 = 1.0 - (r0 * (1.0-u1) / (r2 + r1 * u2))
     rho3 = rho3 / (1.0 - math.pow((r0 * (1.0-u1) / (r2 + r1 * u2)) , N))
-    
-    
+
     
     V1=Vi.GetV_i(1, r0, r1, r2, u1, u2, N)
-    print("V1: {0}".format(V1))
+    #print("V1: {0}".format(V1))
     
     a=N*u1*rho1
     b=N*u1*(1.0-V1-rho1)
     c=N*u2*(1.0-r1/r2)/(1.0-(math.pow((r1/r2) , N)))
+    
+    CacheQ0()
 
-CalculateValues()
+#CalculateValues()
 
 print("a: {0}".format(a))
 print("b: {0}".format(b))
@@ -60,7 +89,9 @@ print("rho1: {0}".format(rho1))
 
 def gee(t):
     global c
-    result = 1.0-math.exp(-(c*t))    
+    #result = 1.0-math.exp(-(c*t))    
+    #result = qk.GetQk(t, r0, r1, r2, u1, u2, N)
+    result = FetchQ0(t)
     return result    
     
 def ell(t):
@@ -72,7 +103,7 @@ def ell(t):
     return result
 
 def traj(t):
-    print("A = {0} B = {1} B/A+B = {2}".format(a,b, b/(a+b)))
+    #print("A = {0} B = {1} B/A+B = {2}".format(a,b, b/(a+b)))
     result = (b/(a+b))*(1.0-math.exp(-(a+b)*t)) + ell(t)
     
     return result
@@ -88,17 +119,17 @@ for i in range(0, datapointcount):
     r1 = ((float(i) / (datapointcount - 1)) * (maxr1 - minr1)) + minr1
     CalculateValues()
     dataPointsT.append(r1)
-    dataPointsY.append(traj(100.0))
-    dataPoints1.append(traj(100.0) - ell(100.0))
-    dataPoints2.append(ell(100.0))
+    dataPointsY.append(traj(t))
+    #dataPoints1.append(traj(t) - ell(t))
+    #dataPoints2.append(ell(t))
     #print(dataPointsY[i])
 #
     
 #t = np.arange(0.,  CurPoint., 1)
 #y=traj(t)
 plt.plot(dataPointsT, dataPointsY, linewidth=4.0, label="X2(t)")
-plt.plot(dataPointsT, dataPoints1, linewidth=1.0, label = "1-exp()")
-plt.plot(dataPointsT, dataPoints2, linewidth=2.0, label = "L(t)")
+#plt.plot(dataPointsT, dataPoints1, linewidth=1.0, label = "1-exp()")
+#plt.plot(dataPointsT, dataPoints2, linewidth=2.0, label = "L(t)")
 plt.legend()
 plt.show()
     
