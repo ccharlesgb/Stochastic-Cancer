5947691be8787fc2700b50ce71fbec89c0beafec
Connor Charles <ccharles.gb@gmail.com>
2014-10-23 17:08:40 +0100

    Tweaked Fixed Point Stuff

--- Code/fixed_point_graphs.py
+++ Code/fixed_point_graphs.py
@@ -15,30 +15,38 @@ def FixedPointStable(r0, r1, u2):
 def FixedPointSaddle(r0,r1, u1, u2):
     return (1.0 - u2)*r1 < (1.0 - u1)*r0
     
-minr1 = 0.0
-maxr1 = 2.0
+minr1 = 0.5
+maxr1 = 1.5
 
-minr2 = 0.0
-maxr2 = 2.0
+minr2 = 0.5
+maxr2 = 1.5
 
-mapSize = 10
+mapSize = 50
 
-mySim = SimTools.Gillespie(10)
+mySim = SimTools.Gillespie(100)
 mySim.r0 = 1.0
 mySim.r1 = 1.0
 mySim.r2 = 1.0
 mySim.u1 = 0.1
 mySim.u2 = 0.1
-mySim.timeLimit = 100000.0
+mySim.timeLimit = 100.0
 
-simsPerDataPoint = 10
+simsPerDataPoint = 50
 
 avgFixTime = np.zeros((mapSize, mapSize))
+
+xticks = np.arange(0, mapSize, 5.0/(mapSize-1.0))
+xlabels = np.arange(minr1, maxr1, 1.0/(mapSize-1.0))
+
+yticks = np.arange(0, mapSize, 5.0/(mapSize-1.0))
+ylabels = np.arange(minr2, maxr2, 1.0/(mapSize-1.0))
 
 for ir1 in range(0, mapSize):
     for ir2 in range(0, mapSize):
         r1 = (float(ir1)/(mapSize-1))*(maxr1-minr1)+minr1
         r2 = (float(ir2)/(mapSize-1))*(maxr2-minr2)+minr2
+        
+        print("r1 = {0} r2 = {1}".format(r1,r2))        
         
         mySim.r1 = r1
         mySim.r2 = r2
@@ -48,6 +56,25 @@ for ir1 in range(0, mapSize):
             mySim.Simulate()
             totalFixTime += mySim.curTime
         
-        avgFixTime[ir1, ir2] = totalFixTime / float(simsPerDataPoint)
+        avgFixTime[ir2, ir1] = totalFixTime / float(simsPerDataPoint)
 
-plt.pcolor(avgFixTime)
+plt.pcolormesh(avgFixTime)
+plt.colorbar()
+plt.xticks(xticks, xlabels)
+plt.yticks(yticks, ylabels)
+plt.xlabel("r1")
+plt.ylabel("r2")
+
+theoryX = []
+theoryDiag = []
+theoryReac = []
+theorySaddle = []
+
+for ir1 in range(0,mapSize):
+    r1 = (float(ir1))*(maxr1-minr1)+minr1
+    r2 = (1-mySim.u2)*r1
+    theoryX.append(r1)
+    theoryDiag.append(r2)
+    
+plt.plot(theoryX, theoryDiag, 'k')
+plt.show()
