386facd9c3c9bc746300a85108b69f2af2e9348c
jnyadams <adams.jonny@gmail.com>
2014-11-13 13:15:02 +0000

    multiple pulse class

--- Code/drugFormalPulsing.py
+++ Code/drugFormalPulsing.py
@@ -11,10 +11,8 @@ import time
 import math
 import numpy
 import multiple_pulse
-        
+import multipulse
 
-
-myPulseParam = PulseParam()
 
 
 #set up simulation parameters
@@ -33,26 +31,46 @@ mySim.in1 = 0
 mySim.in2 = 0
 
 
-
-dataPointCount = 40
-
-simsPerDataPoint = 20
-
-#set some pulsing parameters
-drug_strength=0.6
-freq=1.0
-
 #pick initial r1, to find the starting point in r1/r2 space
-r1_origin=1.25
+r1_origin=1.1
 r2_origin=(1.0-mySim.u2)*r1_origin
-
 mut2= math.pow((1.0-mySim.u2),2) #define a term that appears alot in finding the initial r1 and r2
-#Get the starting r1 and r2 - using formula derived in notebook
-init_r1=math.pow( ( mut2 / (1.0+mut2) ), 0.5) * drug_strength/5.0 + r1_origin
-init_r2=math.pow( (1.0/(1.0+mut2) ) ,0.5)* drug_strength/2.0 + r2_origin
+
+#define frequency for both waves
+frequency = 1.0
 
 
-angle_of_closest_approach=math.atan(1-mySim.u2) #find the angle that gives the closest way to get to 
+#set up multiple pulse parameters
+myPulse= multipulse.Multipulse()
+
+myPulse.drug_strength = 0.6
+myPulse.angle = 0.0
+
+myPulse.init_r1 = math.pow( ( mut2 / (1.0+mut2) ), 0.5) * myPulse.drug_strength/5.0 + r1_origin
+myPulse.init_r2 = math.pow( (1.0/(1.0+mut2) ) ,0.5)* myPulse.drug_strength/2.0 + r2_origin        
+        
+#define r1 pulse paramters
+myPulse.freq_r1 = frequency
+myPulse.r1_width = 1.0
+myPulse.r1_offset = 0.0
+        
+#define r2 pulse parameters       
+myPulse.freq_r2 = frequency
+myPulse.r2_width = 1.0
+myPulse.r2_offset = 0.0
+
+
+mySim.pulseParam=myPulse
+
+mySim.preSim=multipulse.multiple_pulse
+
+#define simulation parameters
+dataPointCount = 20
+simsPerDataPoint = 1000
+
+
+#find the angle that gives the closest way to get to 
+angle_of_closest_approach=math.atan(1-mySim.u2) 
 angle_of_closest_approach*=180/math.pi
 
 #do the simulation
@@ -61,9 +79,11 @@ angleRange=numpy.linspace(0,math.pi/2.0, num=dataPointCount)
 for angle in angleRange:
     #allocate to presim so that the pulse is varied
     
-    mySim.preSim=multiple_pulse.multiple_pulse (mySim,freq, drug_strength, angle, init_r1, init_r2, r1_offset=0.0, r2_offset=0.0, r1_width=1.0, r2_width=1.0)
+    #mySim.preSim=multiple_pulse.multiple_pulse (mySim,freq, drug_strength, angle, init_r1, init_r2, r1_offset=0.0, r2_offset=0.0, r1_width=1.0, r2_width=1.0)
+    myPulse.angle = angle    
+    
+    
     fixTime_term=0.0    
-    print("Angle in radians - supplied to the simulator={0}".format(angle))
     print("Current Data Point = {0}/{1} ({2}%)".format(angle*180/math.pi, 90, 100.0 * float(2.0*angle)/math.pi))
     for i in range(0,simsPerDataPoint):
         mySim.Simulate()
@@ -74,6 +94,8 @@ for angle in angleRange:
             print("WARNING! - system did not fixate")
     average_fix=fixTime_term/simsPerDataPoint
     fixTime.append(average_fix)
+
+
 
 angleRange*=180/math.pi #express agnles in degrees
 plt.plot(angleRange,fixTime)
---
+++ Code/multipulse.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import math
+import TwoSpecies
+
+#Class for simulating cancer dynamics with the Gillespie algorithm
+class Multipulse:
+    def __init__(self):
+        
+        #define general parameters
+        self.drug_strength = 0.0
+        self.angle = 0.0
+        self.init_r1 = 0.0
+        self.init_r2 = 0.0        
+        
+        #define r1 pulse paramters
+        self.freq_r1 = 0.0
+        self.r1_width = 0.0 
+        self.r1_offset = 0.0
+        
+        #define r2 pulse parameters       
+        self.freq_r2 = 0.0
+        self.r2_width = 0.0
+        self.r2_offset = 0.0
+        
+    #helper function to return the contribution to total drug from r1
+    def Get_r1_amp(self):
+        return self.drug_strength*math.cos(self.angle)
+    #helper function to return the contribution to total drug from r2
+    def Get_r2_amp(self):
+        return self.drug_strength*math.sin(self.angle)
+   
+   
+def multiple_pulse(sim):
+    global curTime        
+    param=sim.pulseParam        
+    #set up the r1 pulse function        
+    sim.r1=TwoSpecies.PulseWave(sim.curTime, param.Get_r1_amp(), param.freq_r1, offset=param.r1_offset, width_frac = param.r1_width ) + param.init_r1
+    #set up the r2 pulse function
+    sim.r2=TwoSpecies.PulseWave(sim.curTime, param.Get_r2_amp(), param.freq_r2, offset=param.r2_offset, width_frac = param.r2_width ) + param.init_r2
+    print("r1 is:{0}, r2 is:{1}".format(sim.r1,sim.r2))   
