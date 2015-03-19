e559025a8f6c014cd67c19cbe1f5fce02b8104d7
Connor Charles <ccharles.gb@gmail.com>
2015-03-19 21:40:34 +0000

    Gaussian Movie

---
+++ Code/GaussianMovie.py
@@ -0,0 +1,103 @@
+"""
+A simple example of an animated plot
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+import wright_fisher
+import matplotlib.pyplot as plt
+import math
+
+def GetTauJ(param):
+    summation = 0.0
+    s = param.r[1] - param.r[0]
+    for i in range(0, param.cellTypes):
+        top = math.log(s / (param.u[i] * param.d))
+        bottom = 2.0 * s * math.log(param.N)
+        summation += (top / bottom)
+    return summation;
+
+
+cellTypes = 21
+population = 1e9
+
+myWF = wright_fisher.wright_fisher()
+myHist = wright_fisher.wf_hist(cellTypes)
+myParam = wright_fisher.wright_fisher_params(cellTypes)
+
+myWF.stopAtAppear = 1
+myWF.history = myHist
+myWF.stepLimit = 1000000
+myWF.useApproxTheta = 0
+myWF.params = myParam
+
+myParam.iN[0] = population
+myParam.u = [1e-7] * 10
+myParam.d = 100
+myParam.uNotConst = 0
+
+s = 0.05
+d = 1.8
+
+start = 0.1
+end = 1e-2
+
+sum_s_j = 0.0
+for i in range(0,cellTypes):
+    myParam.r[i] = 1.0 + s * (float(i) / math.sqrt(cellTypes))*(float(i) / math.sqrt(cellTypes))
+    s_j = 0.1 - ((start - end)/((cellTypes-1)**2))*(i**2)
+    sum_s_j += s_j
+    myParam.r[i] = 1.0 + sum_s_j
+    #myParam.r[i] = math.pow(1.0 + s, i)
+    print(s_j)
+
+myWF.Simulate()
+
+def GetDistribution(t):
+    dataY = []
+    for i in range(0, cellTypes): 
+        dataY.append(myHist.histArray[i][t])
+    return dataY
+
+fig, ax = plt.subplots(nrows = 2)
+
+#x = np.arange(0.0, cellTypes, 1.0)
+x = []
+sj_x = []
+for i in range(0, cellTypes):
+    sj_x.append(myParam.r[i] - 1.0)
+    x.append(i)
+print(x)
+print(GetDistribution(0))
+
+line, = ax[0].semilogy(x, GetDistribution(0), 'o-')
+ax[0].set_ylim(10^9, 0)
+line2, = ax[1].semilogy(sj_x, GetDistribution(0), 'o-')
+ax[1].set_ylim(10^9, 0)
+
+ax[0].set_xlabel("j")
+ax[1].set_xlabel("s_j")
+
+ax[0].set_ylabel("N_j")
+ax[1].set_ylabel("N_sj")
+
+#x = np.arange(0, 2*np.pi, 0.01)        # x-array
+#line, = ax.plot(x, np.sin(x))
+
+def animate(i):
+    yDat = GetDistribution(i)
+    line.set_ydata(yDat)  # update the data
+    line2.set_ydata(yDat)
+    return line,line2,
+
+#Init only required for blitting to give a clean slate.
+def init():
+    line.set_ydata(np.ma.array(x, mask=True))
+    line2.set_ydata(np.ma.array(sj_x, mask=True))
+    return line,line2,
+
+
+ani = animation.FuncAnimation(fig, animate, np.arange(0, myWF.curStep,5), init_func=init,
+    interval=50, blit=True)
+plt.show()
--- Code/MATLAB/Data/S2/NewMethodTests/NewMethod.m
+++ Code/MATLAB/Data/S2/NewMethodTests/NewMethod.m
@@ -7,18 +7,21 @@ function [] = NewMethod()
     hold all
     semilogx(dat_01.N, dat_01.Nt_20_anal1, '--');
     semilogx(dat_01.N, dat_01.Nt_20_anal2_transient);
+    semilogx(dat_01.N, dat_01.Nt_20_anal3_neglect);
     xlabel('N')
     ylabel('t_{20}')
     %Error N
     subplot(2,3,4)
     semilogx(dat_01.N, (dat_01.Nt_20-dat_01.Nt_20_anal1)./dat_01.Nt_20, '--g');
     hold all;
-    semilogx(dat_01.N, (dat_01.Nt_20-dat_01.Nt_20_anal2_transient)./dat_01.Nt_20, 'r');
+    semilogx(dat_01.N, abs(dat_01.Nt_20-dat_01.Nt_20_anal2_transient)./dat_01.Nt_20, 'r');
+    semilogx(dat_01.N, abs(dat_01.Nt_20-dat_01.Nt_20_anal3_neglect)./dat_01.Nt_20, 'b');
     hy = graph2d.constantline(0, 'Color',[.7 .7 .7]);
     changedependvar(hy,'y');
     xlim([dat_01.N(1) dat_01.N(length(dat_01.N))])
     xlabel('N')
-    ylabel('Error')
+    ylabel('Absolute Fractional Error')
+    ylim([0 1.0])
     
     %Sweep U
     subplot(2,3,2)
@@ -27,18 +30,21 @@ function [] = NewMethod()
     hold all
     semilogx(dat_02.U, dat_02.Ut_20_anal1, '--');
     semilogx(dat_02.U, dat_02.Ut_20_anal2_transient);
+    semilogx(dat_02.U, dat_02.Ut_20_anal3_neglect);
     xlabel('U')
     ylabel('t_{20}')
     %Error U
     subplot(2,3,5)
     semilogx(dat_02.U, (dat_02.Ut_20-dat_02.Ut_20_anal1)./dat_02.Ut_20, '--g');
     hold all;
-    semilogx(dat_02.U, (dat_02.Ut_20-dat_02.Ut_20_anal2_transient)./dat_02.Ut_20, 'r');
+    semilogx(dat_02.U, abs(dat_02.Ut_20-dat_02.Ut_20_anal2_transient)./dat_02.Ut_20, 'r');
+    semilogx(dat_02.U, abs(dat_02.Ut_20-dat_02.Ut_20_anal3_neglect)./dat_02.Ut_20, 'b');
     hy = graph2d.constantline(0, 'Color',[.7 .7 .7]);
     changedependvar(hy,'y');
     xlim([dat_02.U(1) dat_02.U(length(dat_02.U))])
     xlabel('U')
-    ylabel('Error')
+    ylabel('Absolute Fractional Error')
+    ylim([0 1.0])
 
     %Sweep S
     subplot(2,3,3)
@@ -47,16 +53,24 @@ function [] = NewMethod()
     hold all
     semilogx(dat_03.S, dat_03.St_20_anal1, '--');
     semilogx(dat_03.S, dat_03.St_20_anal2_transient);
+    semilogx(dat_03.S, dat_03.St_20_anal3_neglect);
     xlabel('S')
     ylabel('t_{20}')
+    legend('Simulation', 'Original', 'Transient', 'Neglect');
+    legend('boxoff')
     %Error S
-    subplot(2,3,6)
+    ax = subplot(2,3,6)
     semilogx(dat_03.S, (dat_03.St_20-dat_03.St_20_anal1)./(dat_03.St_20), '--g');
     hold all;
-    semilogx(dat_03.S, (dat_03.St_20-dat_03.St_20_anal2_transient)./(dat_03.St_20), 'r');
+    semilogx(dat_03.S, abs(dat_03.St_20-dat_03.St_20_anal2_transient)./(dat_03.St_20), 'r');
+    semilogx(dat_03.S, abs(dat_03.St_20-dat_03.St_20_anal3_neglect)./(dat_03.St_20), 'b');
     hy = graph2d.constantline(0, 'Color',[.7 .7 .7]);
     changedependvar(hy,'y');
-    xlim([dat_03.S(1) dat_03.S(length(dat_03.S))])
-    xlabel('S')
-    ylabel('Error')
+    xlim([dat_03.S(1) dat_03.S(length(dat_03.S))]);
+    xlabel('S');
+    ylabel('Absolute Fractional Error');
+    ylim([0 1.0])
+    
+    legend('Original', 'Transient', 'Neglect');
+    legend('boxoff')
 end
